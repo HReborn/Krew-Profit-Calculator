6ca16db1857a4c354533dee57070cd4dd039aa6d
Reborn <thehitmanreborng@hotmail.com>
2022-09-14 09:27:45 -0300

    improved the format information was being diplayed

--- src/main/java/com/krew/profitcalculator/dataclasses/ProfitOption.java
+++ src/main/java/com/krew/profitcalculator/dataclasses/ProfitOption.java
@@ -1,5 +1,7 @@
 package com.krew.profitcalculator.dataclasses;
 
+import java.text.DecimalFormat;
+
 public class ProfitOption {
 	private String cargoName;
 	private String destinationIsland;
@@ -33,10 +35,23 @@ public class ProfitOption {
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
+		String profit = String.valueOf(this.profit);
+		// format profit to 15k or keep value if less than 1000
+		if (this.profit > 1000) {
+			profit = String.valueOf(this.profit/100) + "k";
+		} 
+		
+		// format travel time and profit per second to two decimal spots
+		DecimalFormat formatter = new DecimalFormat("#.00");
+		
+		// capitalize destination island and cargo name
+		String destinationIsland = this.destinationIsland.substring(0, 1).toUpperCase() + this.destinationIsland.substring(1);
+		String cargoName = this.cargoName.substring(0, 1).toUpperCase() + this.cargoName.substring(1);
+		
 		return "\nDestination Island: " + destinationIsland +
 			   "\n             Cargo: " + cargoName +
-			   "\n Profit To Be Made: " + profit +
-			   "\n       Travel Time: " + timeSpent + 
-			   "\n Profit per Second: " + profitPerSec + "\n";
+			   "\n Profit To Be Made: " + "$" + profit +
+			   "\n       Travel Time: " + formatter.format(timeSpent) + 
+			   "\n Profit per Second: " + "$" + formatter.format(profitPerSec) + "/s" + "\n";
 	}
 }
